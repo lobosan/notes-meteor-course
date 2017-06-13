cfef81b8a194b1f1848da3bb8867aec02597ca94
lobosan <sp.galindoh@gmail.com>
2017-06-13 10:23:29 -0500

    Init commit

---
+++ client/main.js
@@ -0,0 +1,16 @@
+import ReactDOM from 'react-dom';
+import { Meteor } from 'meteor/meteor';
+import { Tracker } from 'meteor/tracker';
+import { Session } from 'meteor/session';
+
+import { routes, onAuthChange } from '../imports/routes/routes';
+import '../imports/startup/simple-schema-configuration';
+
+Tracker.autorun(() => {
+  const isAuthenticated = !!Meteor.userId();
+  onAuthChange(isAuthenticated);
+});
+
+Meteor.startup(() => {
+  ReactDOM.render(routes, document.getElementById('app'));
+});
